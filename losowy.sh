cff91fa4db15eec3132c06113d1a8c8bd87288a2
jakubkubik98 <mail.mrhishprung@gmail.com>
2023-05-20 12:52:40 +0200

    Jestem git

--- losowy.sh
+++ losowy.sh
@@ -19,3 +19,40 @@ if [ $# -eq 0 ]; then
   show_help
   exit 0
 fi
+
+while [ $# -gt 0 ]
+do
+  case "$1" in
+    -d|--date)
+      show_date
+      ;;
+    -l|--logs)
+      if [ -n "$2" ] && [ "$2" -eq "$2" ] 2>/dev/null; then
+        create_logs $2
+        shift
+      else
+        create_logs 100
+      fi
+      ;;
+    -h|--help)
+      show_help
+      ;;
+    -e|--error)
+      if [ -n "$2" ] && [ "$2" -eq "$2" ] 2>/dev/null; then
+        create_errors $2
+        shift
+      else
+        create_errors 100
+      fi
+      ;;
+    --init)
+      git clone <adres_repozytorium> && export PATH=$PATH:$(pwd)
+      ;;
+    *)
+      echo "Nieznana opcja: $1"
+      show_help
+      exit 1
+      ;;
+  esac
+  shift
+done
